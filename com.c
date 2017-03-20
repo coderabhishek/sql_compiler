c25fdb0fee6cc77cdf108b33f35304ac604a6875
coderabhishek <abhishekkmr18@gmail.com>
2017-03-20 19:11:05 +0530

    no idea if it works

--- com.c
+++ com.c
@@ -1,22 +1,29 @@
+#include<string.h>
 #include<stdio.h>
 #include<stdlib.h>
+
 #include "com.h"
 #define true 1
 #define false 0
 
 int ch = 0;
 int maxp = 0;
-
-int up(){
-	printf("MX --> %d\n", maxp);
-	maxp = (maxp>ptr?maxp:ptr);
-}
+node *memory[100];
+int mem_ptr = 0;
 
 typedef short int bool;
 
 char input[1000000];
 int ptr = 0;
 
+
+
+int up(){
+	ptr++;
+//	printf("MX --> %d\n", maxp);
+	maxp = (maxp>ptr?maxp:ptr);
+	return ptr-1;
+}
 void sanitize(){
 	int r=0, w=0;
 	while(input[r]!='\0'){
@@ -27,272 +34,584 @@ void sanitize(){
 	input[w] = '\0';
 }
 
-bool match(char *s, int i){
+node *match(char *s, int i){
 	if(ch) printf("==================================\n\n%s\n\n", s);
 	while(input[ptr]==' ' || input[ptr] == '\t')
-		ptr++;
+		up();
 	char test[i];
 	int j;
 	for(j=0;j<i && input[ptr]!='\0' && input[ptr]!=' ';++j){
 		if(ch) printf("&& %c &&\n", input[ptr]);
-		test[j] = input[ptr++];
+		test[j] = input[up()];
 	}
 	test[j++] = '\0';	
 	if(ch) printf("\n\n==============================================\n\n");
-	return (strcmp(test, s)==0);
+	if(strcmp(test, s)==0)
+		return create(s);
+	else return NULL;
 }
 
-bool constant_val(){
+node *constant_val(){
+	char val[100];
+	int i=0;
 	while(input[ptr]==' ' || input[ptr] == '\t')
-		ptr++;
-	if(input[ptr++]!='"')
-		return false;
+		up();
+	if(input[up()]!='"')
+		return NULL;
 	while(input[ptr]!='"'){
 		if(ch) printf("Constant val char-> %c\n", input[ptr]);
 		if(input[ptr] == '\0' || input[ptr] == ' ')
-			return false;
-		ptr++;
+			return NULL;
+		val[i++] = input[ptr];
+		up();
 	}
-	ptr++;
-	return true;
+	val[i] = '\0';
+	up();
+	return create(val);
 }
 
-bool numeric_val(){
+node * numeric_val(){
+	char val[100];
+	int i=0;
 	while(input[ptr]==' ' || input[ptr] == '\t')
-		ptr++;
-	if(!isdigit(input[ptr])) return false;
+		up();
+	if(!isdigit(input[ptr])) return NULL;
 	while(isdigit(input[ptr])){
 	//	if(ch) printf("digit %c\n", input[ptr]);
-		ptr++;
+		val[i++] = input[ptr];
+		up();
 	}
-	return true;
+	val[i] = '\0';
+	return create(val);
 }
 
-bool v_val(){
+node * v_val(){
 	int save = ptr;
-	int fl = constant_val();
+	node *fl = constant_val();
 	if(fl)
-		return true;
+		return fl;
 	else {
 		ptr = save;
-		int temp = numeric_val();
+		node *temp = numeric_val();
 	//	if(ch) printf("050505005050#$%c %c\n", input[ptr-1], input[ptr]);
 		return temp;
 	}
 }
 
-bool COL(){
+node * COL(){
 	while(input[ptr]==' ' || input[ptr] == '\t')
-		ptr++;
-	return (input[ptr++] == ';');
+		up();
+	if(input[up()] == ';')
+		return create(";");
+	else return NULL;
 	
 }
 
 
-bool identifier(){
+node * identifier(){
+	char val[100];
+	int i=0;
 	while(input[ptr]==' ' || input[ptr] == '\t')
-		ptr++;
+		up();
 	if(ch) printf("IDENTIFIER  %c\n\n", input[ptr]);
 
 	int check = ptr;
-	if(!isalpha(input[ptr++]))
-		return false;
+	if(!isalpha(input[up()]))
+		return NULL;
 
 	while(isalnum(input[ptr])){
 		if(ch) printf(" ** %c **\n", input[ptr]);
-		ptr++;
+		val[i++] = input[ptr];
+		up();
 	}
-	return true;
+	val[i] = '\0';
+	return create(val);
 }
 
-bool COMMA(){
+node * COMMA(){
 	while(input[ptr]==' ' || input[ptr] == '\t')
-		ptr++;
-	return (input[ptr++] == ',');
+		up();
+	if(input[up()] == ',')
+		return create(",");
+	else NULL;
 }
 
 
-bool BO(){
+node * BO(){
 	while(input[ptr]==' ' || input[ptr] == '\t')
-		ptr++;
+		up();
 	if(ch) printf("BO  %c\n", input[ptr]);
-	return (input[ptr++] == '(');
+	if(input[up()] == '(')
+		return create("(");
+	else NULL;
 	
 }
-bool BC(){
+node * BC(){
 	//	if(ch) printf("BBBBBBCCCCCCCCCCCCCCC!!\n");
 	while(input[ptr]==' ' || input[ptr] == '\t')
-		ptr++;
-	return (input[ptr++] == ')');
+		up();
+	if(input[up()] == ')')
+		return create(")");
+	else NULL;
 }
 
-bool WILDCARD(){
+node * WILDCARD(){
 
 	while(input[ptr]==' ' || input[ptr] == '\t')
-		ptr++;	
-	return (input[ptr++] == '*');
+		up();	
+	if(input[up()] == '*')
+		return create("*");
+	else NULL;
 }
 
-bool id_list(){
+node * id_list(){
 	int save = ptr;
-	return ((ptr = save, (identifier() && COMMA() && id_list())) || ((ptr = save, identifier())));
+	node *n1, *n2, *n3;
+	if((n1 = identifier()) &&  (n2 = COMMA()) && (n3 = id_list())){
+		node *nd = create("ID_LIST");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3; 
+		return nd;
+	}
+	ptr = save;
+	if(n1 = identifier()){
+		node *nd = create("ID_LIST");
+		nd->child[(nd->cptr++)] = n1; 
+		return nd;
+	}
+	return NULL;
 }
 
-bool tab_list(){
+node * tab_list(){
 	//if(ch) printf("&&\n\n%s\n\n&&", input);
 	int save = ptr;
-	return ((ptr = save, id_list()) || (ptr = save, (BO() && selection2() && BC())) || (ptr = save, (BO() && sel() && BC())));
+	node *n1, *n2, *n3;
+	if(n1 = id_list()) return n1;	
+	
+	ptr = save;
+	if((n1 = BO()) && (n2 = selection2()) && ( n3 = BC()))
+	{
+		node *nd = create("TAB_LIST");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		return nd;
+	}	
+	ptr = save;
+	if((n1=BO()) && (n2=sel()) && (n3=BC())){
+		node *nd = create("TAB_LIST");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		return nd;
+	}
+	return NULL;
 }
 
-bool columns(){
+node * columns(){
 	int save = ptr;
-	return ((ptr = save, WILDCARD()) || (ptr = save, id_list())); 
+	node *n1, *n2;
+	if((n1 =  WILDCARD())){
+		return 	create("*");
+	}
+	ptr = save;
+	if((n1 = id_list())){
+		node *nd = create("COLUMNS");
+		nd->child[(nd->cptr++)] = n1;
+		return nd;	
+	}	
+	return NULL;
 }
 
 
 
-bool query(){
+node * query(){
 	//if(ch) printf("++    %c %c %c\n", input[ptr], input[ptr+1], input[ptr+2]);
-	return (columns() && match("FROM", 4) && tab_list());
+	node *n1, *n2, *n3;
+	if((n1=columns()) && (n2=match("FROM", 4)) && (n3=tab_list())){
+		node *nd = create("QUERY");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		return nd;
+	}
+	return NULL;
 }
 
-bool sel(){
-	return (match("SELECT", 6) && query());
+node * sel(){
+	node *n1, *n2;
+	if((n1=match("SELECT", 6)) && (n2=query())){
+		node *nd = create("TAB_LIST");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;	
+		return nd;	
+	}
+	return NULL;
 }
 
 
-bool updation(){
-	return(match("UPDATE", 6) && identifier() && match("SET", 3) && updation_list() && match("WHERE", 5) && constraint() && COL());
+node * updation(){
+	node *n1, *n2, *n3, *n4, *n5, *n6, *n7;
+	if((n1=match("UPDATE", 6)) && (n2=identifier()) && (n3=match("SET", 3)) && (n4=updation_list()) && (n5=match("WHERE", 5)) && (n6=constraint()) && (n7=COL())){
+		node *nd = create("UPDATE");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		nd->child[(nd->cptr++)] = n4;
+		nd->child[(nd->cptr++)] = n5;
+		nd->child[(nd->cptr++)] = n6;
+		nd->child[(nd->cptr++)] = n7;
+		return nd;
+	}
+	return NULL;
 }
 
-bool updation_list(){
+node * updation_list(){
 	int save = ptr;
-	return((ptr = save, identifier() && match("=", 1) && v_val() && COMMA() && updation_list()) || (ptr = save, identifier() && match("=", 1) && v_val()));
+	node *n1, *n2, *n3, *n4, *n5;
+	if((n1=identifier()) && (n2=match("=", 1)) && (n3=v_val()) && (n4=COMMA()) && (n5=updation_list())){
+		node *nd = create("UPDATE_LIST");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		nd->child[(nd->cptr++)] = n4;
+		nd->child[(nd->cptr++)] = n5;
+		return nd;
+	}	
+       ptr = save;
+       if((n1 = identifier()) && (n2 = match("=", 1)) && (n3 = v_val())){
+       	node *nd = create("UPDATE_LIST");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		return nd;
+       }
+       return NULL;
+}
+
+node *n1, *n2, *n3, *n4, *n5, *n6, *n7, *n8;
+node * deletion(){
+	if((n1=match("DELETE", 8)) && (n2=match("FROM", 4)) && (n3=identifier()) && (n4=match("WHERE", 5)) && (n5=constraint()) && (n6=COL())){
+		node *nd = create("DELETION");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		nd->child[(nd->cptr++)] = n4;
+		nd->child[(nd->cptr++)] = n5;
+		nd->child[(nd->cptr++)] = n6;
+		return nd;
+	}
+	return NULL;
+}
+
+node * alter_drop(){
+	if((n1= match("ALTER", 5)) && (n2=match("TABLE", 5)) && (n3=identifier()) && (n4=match("DROP", 4)) && (n5=match("COLUMN", 6)) && (n6=identifier()) && (n7=COL()))
+	{
+node *nd = create("ALTER_DROP");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		nd->child[(nd->cptr++)] = n4;
+		nd->child[(nd->cptr++)] = n5;
+		nd->child[(nd->cptr++)] = n6;
+		nd->child[(nd->cptr++)] = n7;
+		return nd;
+	}
+	return NULL;
+}
+
+node * alter_add(){
+	node *n9;
+	if((n1=match("ALTER", 5)) && (n2=match("TABLE", 5)) && (n3=identifier()) && (n4=match("ADD", 3)) && (n5=match("COLUMN", 6)) && (n6=identifier()) && (n7=identifier()) && (n8=constraint()) && (n9=COL())){
+		node *nd = create("ALTER_ADD");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		nd->child[(nd->cptr++)] = n4;
+		nd->child[(nd->cptr++)] = n5;
+		nd->child[(nd->cptr++)] = n6;
+		nd->child[(nd->cptr++)] = n7;
+		nd->child[(nd->cptr++)] = n8;
+		nd->child[(nd->cptr++)] = n9;
+		return nd;	
+	}
+	return NULL;
 }
 
 
-bool deletion(){
-	return(match("DELETE", 8) && match("FROM", 4) && identifier() && match("WHERE", 5) && constraint() && COL());
-}
-
-bool alter_drop(){
-	return(match("ALTER", 5) && match("TABLE", 5) && identifier() && match("DROP", 4) && match("COLUMN", 6) && identifier() && COL());
-}
-
-bool alter_add(){
-	return(match("ALTER", 5) && match("TABLE", 5) && identifier() && match("ADD", 3) && match("COLUMN", 6) && identifier() && identifier() && constraint() && COL());
-}
-
-
-bool value_list(){
+node * value_list(){
 	int save = ptr;
-	return((ptr = save, v_val() && COMMA() && value_list()) || (ptr = save, v_val()));
+	if((n1=v_val()) && (n2=COMMA()) && (n3=value_list())){
+		node *nd = create("VALUE_LIST");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		return nd;
+	}	
+	ptr = save;
+	if(n1 = v_val()){
+		node *nd = create("ALTER_ADD");
+		nd->child[(nd->cptr++)] = n1;
+		return nd;
+	}
+	return NULL;
+
 }
 
-bool fields(){
-	return(BO() && id_list() && BC());
+node * fields(){
+	if((n1=BO()) && (n2=id_list()) && (n3=BC())){
+		node *nd = create("FIELDS");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		return nd;
+	}
+	return NULL;
 }
 
 
-bool vb(){
+node * vb(){
 	int save = ptr;
-	return ((ptr = save, fields() && match("VALUES", 6)) || (ptr = save, match("VALUES", 6)));
+	if((n1=fields()) && (n2=match("VALUES", 6))){
+		node *nd = create("VB");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		return nd;
+	}	 
+	ptr = save;
+	if((n1 = match("VALUES", 6))){
+		return n1;
+	}
+	return NULL;
 }
 
 
-bool insertion(){
-	return(match("INSERT", 6) && match("INTO", 4) && identifier() && vb() && BO() && value_list() && BC() && COL());
+node * insertion(){
+	if((n1=match("INSERT", 6)) && (n2=match("INTO", 4)) && (n3=identifier()) && (n4=vb()) && (n5=BO()) && (n6=value_list()) && (n7=BC()) && (n8=COL())){
+		node *nd = create("INSERTION");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		nd->child[(nd->cptr++)] = n4;
+		nd->child[(nd->cptr++)] = n5;
+		nd->child[(nd->cptr++)] = n6;
+		nd->child[(nd->cptr++)] = n7;
+		nd->child[(nd->cptr++)] = n8;
+		return nd;
+	}
+	return NULL;
 }
 
 
 
 
-bool schema_list(){
+node * schema_list(){
 //	if(ch) printf("SCHEMA LIST %c%c%c%c\n", input[ptr], input[ptr+1], input[ptr+2], input[ptr+3]);
 	
 	int save = ptr;
-	return((ptr = save, identifier() && identifier() && constraint() && COMMA() && schema_list()) || (ptr = save, identifier() && identifier() && constraint()));
+	if((n1=identifier()) && (n2=identifier()) && (n3=constraint()) && (n4=COMMA()) && (n5=schema_list())){
+		node *nd = create("SCHEMA_LIST");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		nd->child[(nd->cptr++)] = n4;
+		nd->child[(nd->cptr++)] = n5;
+		return nd;
+		
+	}	
+	ptr = save;
+	if((n1=identifier()) && (n2=identifier()) && (n3=constraint())){
+		node *nd = create("SCHEMA_LIST");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		return nd;
+	}
+	return NULL;
 }
 
 
-bool creation(){
-	return(match("CREATE", 6) && match("TABLE", 5) && identifier() && BO() && schema_list() && BC() && COL());
+node * creation(){
+	if((n1=match("CREATE", 6)) && (n2=match("TABLE", 5)) && (n3=identifier()) && (n4=BO()) && (n5=schema_list()) && (n6=BC()) && (n7=COL())){
+		node *nd = create("CREATION");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		nd->child[(nd->cptr++)] = n4;
+		nd->child[(nd->cptr++)] = n5;
+		nd->child[(nd->cptr++)] = n6;
+		nd->child[(nd->cptr++)] = n7;
+		return nd;
+	}
+	return NULL;
 }
 
-bool selection(){
+node * selection(){
 	int save = ptr;
-	return ((ptr = save, selection1()) || (ptr = save, selection2()) || (ptr = save, selection3()));
+	if(n1=selection1()){
+		return n1;
+	}	
+	ptr = save;
+	if(n1 = selection2()){
+		return n1;
+	}
+	ptr = save;
+       if(n1=selection3()) return n1;
+	return NULL;
 }
 
-bool selection1(){
-	return (sel() && match("WHERE", 5) && constraint() && COL());
+node * selection1(){
+	if((n1=sel()) && (n2=match("WHERE", 5)) && (n3=constraint()) && (n4=COL())){
+		node *nd = create("SELECTION1");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		nd->child[(nd->cptr++)] = n4;
+		return nd;	
+	}
+	return false;
 }
 
-bool selection2(){
-	return (sel() && match("WHERE", 5) && constraint());
+node * selection2(){
+	if((n1=sel()) && (n2=match("WHERE", 5)) && (n3=constraint())){
+		node *nd = create("SELECTION2");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		return nd;			
+	}
+	return NULL;
 }
 
-bool selection3(){
-	return (sel() && COL());
+node * selection3(){
+	if((n1=sel()) && (n2=COL())){
+		node *nd = create("SELECTION3");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		return nd;	
+	}
+	return NULL;
 }
 
 
-bool drop(){
-	return(match("DROP", 4) && match("TABLE", 5) && identifier() && COL());
+node * drop(){
+	if((n1=match("DROP", 4)) && (n2=match("TABLE", 5)) && (n3=identifier()) && (n4=COL())){
+		node *nd = create("SELECTION2");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		nd->child[(nd->cptr++)] = n4;
+		return nd;	
+	}
+	return NULL;
 }
 
-bool line(){
+short int line(){
 	int save = ptr;
-	return((up(), ptr = 0, selection()) || 
-			(up(), ptr = 0, drop()) ||
-			(up(), ptr = 0, deletion()) ||
-			(up(), ptr = 0, updation()) ||
-			(up(), ptr = 0, creation()) ||
-			(up(), ptr = 0, alter_add()) ||
-			(up(), ptr = 0, alter_drop()) ||
-			(up(), ptr = 0, insertion())
+	return((ptr = 0, selection()) || 
+			(ptr = 0, drop()) ||
+			(ptr = 0, deletion()) ||
+			(ptr = 0, updation()) ||
+			(ptr = 0, creation()) ||
+			(ptr = 0, alter_add()) ||
+			(ptr = 0, alter_drop()) ||
+			(ptr = 0, insertion())
 	  );
 }
 
-bool CONJUNCTION(){
+node * CONJUNCTION(){
 	int save = ptr;
-	bool f = match("AND", 3);
-	if(f) return true;
+	node *f = match("AND", 3);
+	if(f) return f;
 	ptr = save;
 	return match("OR", 2);
 }
 
-bool COMPARATOR(){
-	bool f = (input[ptr] == '>' || input[ptr] == '<' || input[ptr]=='=');
-	ptr++;
+node * COMPARATOR(){
+	node *f = NULL;
+	if(input[ptr] == '>' || input[ptr] == '<' || input[ptr]=='='){
+		char val[5];
+		val[0] = input[ptr];
+		val[1] = '\0';
+		f = create(val);
+	}
+	up();
 	//	if(ch) printf("%c 9090  %d\n", input[ptr], f);
 	return f;
 }
 
-bool condition(){
+node * condition(){
 	int save = ptr;
-	return ((ptr = save, (identifier() && COMPARATOR() && identifier())) || (ptr = save, (v_val() && COMPARATOR() && v_val())) || (ptr = save, (v_val() && COMPARATOR() && identifier())) || (ptr = save, (identifier() && COMPARATOR() && v_val())));
+	if((n1=identifier()) && (n2=COMPARATOR()) && (n3=identifier())){
+		node *nd = create("SELECTION2");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		return nd;
+	}	
+	ptr = save;
+       if((n1=v_val()) && (n2=COMPARATOR()) && (n3=v_val())){
+       	node *nd = create("SELECTION2");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		return nd;
+       } 
+       ptr = save;
+       if((n1=v_val()) && (n2=COMPARATOR()) && (n3=identifier())){
+       	node *nd = create("SELECTION2");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		return nd;
+       } 
+       ptr = save;
+       if((n1=identifier()) && (n2=COMPARATOR()) && (n3=v_val())){
+       	node *nd = create("SELECTION2");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		return nd;
+       }
+       return NULL;
 }
 
-bool constraint(){
+node * constraint(){
 //	if(ch) printf("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOoo            %c\n\n", input[ptr+1]);
 
 	int save = ptr;
-	return ((ptr = save, (condition() && CONJUNCTION() && constraint())) || (ptr = save, condition()));	
+	if((n1=condition()) && (n2=CONJUNCTION()) && (n3=constraint())){
+		node *nd = create("SELECTION2");
+		nd->child[(nd->cptr++)] = n1;
+		nd->child[(nd->cptr++)] = n2;
+		nd->child[(nd->cptr++)] = n3;
+		return nd;
+	}	
+	ptr = save;
+	if(n1=condition()){
+		return condition();
+	}	
+	return NULL;
 }
 
 int main(){
-	while(1){
+	int l_no = 0;
+	while(gets(input)){
+		maxp = 0;
 		ptr = 0;
-		gets(input);
+///		gets(input);
 		//sanitize();
 		if(ch) printf("PARSING:--> %s\n\n", input);
 		int save = 0;
 		int res = line();
 		printf("%d ", res);
+		l_no++;
 		if(res == 0)
-			printf("  %c%c%c", input[ptr-1], input[ptr], input[ptr+1]); 
+			printf("SYNTAX ERROR near:  \"%c%c%c\"  line no. : %d  col: %d ", input[maxp-1], input[maxp], input[maxp+1], l_no, maxp);
+	       	
 //		if(ch) printf("--  %c %c\n", input[ptr-1], input[ptr]);
+		printf("\n\n");
 	}
 	return 0;
 }
--- com.h
+++ com.h
@@ -1,27 +1,38 @@
 /* From 'com.c': */
-
-short int match (char *s , int i );
+typedef struct node{ 
+          int cptr; 
+          struct node *child[50]; 
+          char s[50]; 
+  } node; 
+   
+node *create(char *s){ 
+        node *nd = (node *)malloc(sizeof(node)); 
+        strcpy(nd->s, s); 
+        nd->cptr = 0; 
+        return nd; 
+ }
+node * match (char *s , int i );
 //#include "c-extensions.h"
-short int updation_list();
-short int COL  (void);
-short int identifier  (void);
-short int COMMA  (void);
-short int BO  (void);
-short int BC  (void);
-short int WILDCARD  (void);
-short int id_list  (void);
-short int tab_list  (void);
-short int columns  (void);
-short int query  (void);
-short int sel  (void);
-short int selection  (void);
-short int selection1  (void);
-short int selection2  (void);
-short int selection3  (void);
-short int selection4  (void);
-short int drop  (void);
+node * updation_list();
+node * COL  (void);
+node * identifier  (void);
+node * COMMA  (void);
+node * BO  (void);
+node * BC  (void);
+node * WILDCARD  (void);
+node * id_list  (void);
+node * tab_list  (void);
+node * columns  (void);
+node * query  (void);
+node * sel  (void);
+node * selection  (void);
+node * selection1  (void);
+node * selection2  (void);
+node * selection3  (void);
+node * selection4  (void);
+node * drop  (void);
 short int line  (void);
-short int CONJUNCTION  (void);
-short int COMPARATOR  (void);
-short int condition  (void);
-short int constraint  (void);
+node * CONJUNCTION  (void);
+node * COMPARATOR  (void);
+node * condition  (void);
+node * constraint  (void);
